4a8f1e63da9d887356186e98274f15324c056b2a
cjk101812 <cullen.j.kirkpatrick@gmail.com>
2015-10-19 20:24:34 -0400

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ckappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ckappApp
+ */
+angular.module('ckappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
